7880b3345ada9772baf54f2ce824f3ca31540e7b
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2023-02-08 09:16:52 +0530

    Create looping statements.cpp

---
+++ looping statements.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b,c;
+    cout<<"Enter the value till which you want to print the number : ";
+    cin>>b;
+    for(a=1;a<=b;a++){
+        cout<<a<<" ";
+    do{
+        cout<<"-Hello\n";
+        break;
+    }while(a!=b);
+    }
+}
